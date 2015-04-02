a3f32f140486dc974d4efc06fd6eb723e7066b8d
Stratigos <Stratigos@users.noreply.github.com>
2015-04-02 17:30:59 -0400

    expands on app - intro to passing request through middleware methods

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 /**
- * example use of express. simple app that listens on port 8000 for HTTP POST
- *  and prints a sentence.
+ * example use of express. under construction is an app which listens for
+ *  messages in the form of new badges earned from www.codeschool.com, and
+ *  sends them to a subsocket.
  */
 'use strict';
 
 var express = require('express'); // import express library
 var app     = express();          // create instance of express
+var badges  = require('./controllers/badges');
 
 // app.use() declares middleware for the request before routing occurrs
 //  express.json() causes the request body to be parsed as JSON
 app.use(express.json());
 
 // route for POST request to '/'
-app.post('/', function(req, res){
-    res.send("\n\nrubber baby buggy bumpers\n\n"); // send() text to the response
-});
+//  'badges.save' and 'badges.send' represent middleware and method
+//    (i.e., "controller"."action"). the save() method will be evoked on
+//    request. when save() completes, the request will pass through send().
+app.post('/', badges.save, badges.send);
+
+// the following is an example of a view render, if this app were doing
+//  more than parsing JSON.
+// app.post('/', badges.save, badges.send, function(req, res){ // (request, response)
+//     // res.render('dashboard'); // render a view
+// });
 
 app.listen(8000); // listen for requests to this server on port 8000
---
+++ controllers/badges.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * save an incoming badge
+ * @todo incomplete
+ */
+exports.save = function(){};
+
+/**
+ * 
+ * @todo incomplete
+ */
+exports.send = function(){};
