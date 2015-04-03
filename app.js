30e47e8c2797781c495e2b56ed98d806f30560c9
Stratigos <Stratigos@users.noreply.github.com>
2015-04-02 23:10:14 -0400

    better message returned from app

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 //    (i.e., "controller"."action"). the save() method will be evoked on
 //    request. when save() completes, the request will pass through send().
 app.post('/', badges.save, badges.send, function(req, res) {
-    res.send("\ndone\n\n");
+    res.send("\nrequest success, response complete\n\n");
 });
 
 // the following is an example of a view render, if this app were doing
